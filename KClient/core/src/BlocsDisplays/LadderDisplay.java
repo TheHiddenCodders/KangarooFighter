9332ea417648da7f888addf85ae58876c2774821
sebastienservouze <sebastien.servouze@gmail.com>
2016-02-11 18:39:46 +0100

    Update search by rank

--- KClient/core/src/BlocsDisplays/LadderDisplay.java
+++ KClient/core/src/BlocsDisplays/LadderDisplay.java
@@ -7,6 +7,7 @@ import Packets.SearchLadderPacket;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -14,6 +15,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class LadderDisplay extends Display 
@@ -193,11 +195,17 @@ public class LadderDisplay extends Display
 				prev.setText("-");
 				prev.setTouchable(Touchable.disabled);
 			}
+			else
+			{
+				prev.setText("<");
+				prev.setTouchable(Touchable.enabled);
+			}
 			
 			// Set parameters of buttons
 			me.setPosition(next.getX() + next.getWidth(), 5);	
 			top.setPosition(me.getX() + me.getWidth(), 5);
 			friends.setPosition(top.getX() + top.getWidth(), 5);
+			
 			rankField.setText(String.valueOf(rankMid));
 			
 			/*
@@ -205,13 +213,23 @@ public class LadderDisplay extends Display
 			 * So he can send to the client the right ladder packet
 			 */
 			
+			// Search listener
 			search.addListener(new ClickListener()
 			{
 				@Override
 				public void clicked(InputEvent event, float x, float y) 
 				{
 					SearchLadderPacket packet = new SearchLadderPacket();
-					packet.name = nameField.getText();
+					
+					if (!nameField.getText().isEmpty())
+						packet.name = nameField.getText();
+					
+					else if (!rankField.getText().isEmpty())
+						packet.pos = Integer.valueOf(rankField.getText());
+					
+					else
+						packet.name = homeStage.main.player.getName();
+					
 					homeStage.main.network.send(packet);
 					super.clicked(event, x, y);
 				}
