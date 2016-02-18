2d2228aa7ef62d53aaa54ed31388f1c1eb6b909a
Kurond <charlymoreau30@gmail.com>
2016-02-18 19:36:50 +0100

    Add GameServerPacket

--- KClient/core/src/Packets/GameServerPacket.java
+++ KClient/core/src/Packets/GameServerPacket.java
@@ -8,24 +8,16 @@ public class GameServerPacket extends Packets
 	 */
 	private static final long serialVersionUID = -3276163525805452816L;
 	
-	public String ip;
-	public float x, y;
-	public int health;
-	public int damage;
-	public int state;
-	public boolean flip;
+	public KangarooPacket player;
+	public KangarooPacket opponent;
+	
+	public float time;
 
 	@Override
 	public String toString()
 	{
 		return super.toString()
 				+ "\n"
-				+ "- [ip]: " + ip + "\n"
-				+ "- [x]: " + x + "\n"
-				+ "- [y]: " + y + "\n"
-				+ "- [health]: " + health + "\n"
-				+ "- [damage]: " + damage + "\n"
-				+ "- [state]: " + state + "\n"
-				+ "- [flip]: " + flip + "\n";
+				+ "- [time]: " + time + "\n";
 	}
 }
